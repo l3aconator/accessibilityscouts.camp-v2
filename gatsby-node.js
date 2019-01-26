3a3ef1b2c8dd2de6b09860f81161c143f93e65a3
Kyle Bacon <hello@kylebaconcreative.com>
2019-01-26 09:34:41 -0800

    Rename file with index, load with gatsby node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,47 +4,227 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  return graphql(
-    `
-      {
-        allMarkdownRemark(limit: 1000) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
+  return graphql(`
+    {
+      allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/about/" } }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
             }
           }
         }
       }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
     }
+  `)
+    .then(result => {
+      if (result.errors) {
+        result.errors.forEach(e => console.error(e.toString()))
+        return Promise.reject(result.errors)
+      }
 
-    // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+      const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+      const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+      posts.forEach((post, index) => {
+        const previous =
+          index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
+        createPage({
+          path: `/about${post.node.fields.slug}`,
+          component: blogPostTemplate,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
+        })
       })
     })
-  })
+    .then(() => {
+      return graphql(`
+        {
+          allMarkdownRemark(
+            filter: { fileAbsolutePath: { regex: "/interaction/" } }
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                }
+              }
+            }
+          }
+        }
+      `)
+    })
+    .then(result => {
+      if (result.errors) {
+        result.errors.forEach(e => console.error(e.toString()))
+        return Promise.reject(result.errors)
+      }
+
+      const workPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+      const posts = result.data.allMarkdownRemark.edges
+
+      posts.forEach((post, index) => {
+        const previous =
+          index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
+
+        createPage({
+          path: `/interaction${post.node.fields.slug}`,
+          component: workPostTemplate,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
+        })
+      })
+    })
+    .then(() => {
+      return graphql(`
+        {
+          allMarkdownRemark(
+            filter: { fileAbsolutePath: { regex: "/badge/" } }
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                }
+              }
+            }
+          }
+        }
+      `)
+    })
+    .then(result => {
+      if (result.errors) {
+        result.errors.forEach(e => console.error(e.toString()))
+        return Promise.reject(result.errors)
+      }
+
+      const workPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+      const posts = result.data.allMarkdownRemark.edges
+
+      posts.forEach((post, index) => {
+        const previous =
+          index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
+
+        createPage({
+          path: `/badge${post.node.fields.slug}`,
+          component: workPostTemplate,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
+        })
+      })
+    })
+    .then(() => {
+      return graphql(`
+        {
+          allMarkdownRemark(
+            filter: { fileAbsolutePath: { regex: "/sound/" } }
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                }
+              }
+            }
+          }
+        }
+      `)
+    })
+    .then(result => {
+      if (result.errors) {
+        result.errors.forEach(e => console.error(e.toString()))
+        return Promise.reject(result.errors)
+      }
+
+      const workPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+      const posts = result.data.allMarkdownRemark.edges
+
+      posts.forEach((post, index) => {
+        const previous =
+          index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
+
+        createPage({
+          path: `/sound${post.node.fields.slug}`,
+          component: workPostTemplate,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
+        })
+      })
+    })
+    .then(() => {
+      return graphql(`
+        {
+          allMarkdownRemark(
+            filter: { fileAbsolutePath: { regex: "/vision/" } }
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                }
+              }
+            }
+          }
+        }
+      `)
+    })
+    .then(result => {
+      if (result.errors) {
+        result.errors.forEach(e => console.error(e.toString()))
+        return Promise.reject(result.errors)
+      }
+
+      const workPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+      const posts = result.data.allMarkdownRemark.edges
+
+      posts.forEach((post, index) => {
+        const previous =
+          index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
+
+        createPage({
+          path: `/vision${post.node.fields.slug}`,
+          component: workPostTemplate,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
+        })
+      })
+    })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
